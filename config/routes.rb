f68cf963575c1d300749e24be38bf2c75b099219
Daniel Chang <daniel.chang85@gmail.com>
2013-11-05 14:31:08 -0500

    fixed index redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AssocTest::Application.routes.draw do
   resources :customers
-
+  root 'customers#index' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
