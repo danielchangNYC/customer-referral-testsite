82276ab24eff8da438e4564928ef3c4d4e7ff532
Daniel Chang <daniel.chang85@gmail.com>
2013-11-06 01:11:15 -0500

    refactored customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :referrals, class_name: "Customer", foreign_key: "referring_customer_id"
-  belongs_to :referring_customer, class_name: "Customer", foreign_key: "referring_customer_id"
+  belongs_to :referring_customer, class_name: "Customer"
 end
